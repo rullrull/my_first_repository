46df9b8d9ce84f763a18beda3b5f133273fa1cb8
rullrull <143788468+rullrull@users.noreply.github.com>
2023-09-11 17:50:24 +0900

    Add files via upload

---
+++ var.py
@@ -0,0 +1,186 @@
+my_var = 10
+print(my_var)
+my_list = [1, 2, 3]
+print(*my_list)
+
+#정수
+my_int = 10
+#부동 소수점
+my_float = 3.14
+#복소수
+my_complex = 3 +4j
+
+my_character = 'A'
+my_char = "@"
+
+my_string ='Hello, Wold!'
+str_name = "python"
+
+my_bool = True
+bFlag = False
+
+my_list = [1, 2, 'three', True]
+lsElement = [3.14, "b", 'two', False]
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
+
+print(my_list.__len__())
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list)
+
+print(my_list[3])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list)
+
+print(my_list[3])
+
+list_el = my_list[2]
+
+print(list_el)
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list)
+
+my_list[3] = 11
+
+print(my_list)
+
+my_list = [10,3, 8, 9, 42, "hello"]
+print(my_list)
+
+n_add = my_list[3] + my_list[2]
+print(n_add)
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list[2:5])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list[:3])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list[0:3])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list[4:])
+
+my_list = [10, 3, 8, 9, 42, "hello", [5, 4, 2, "world"]]
+print(my_list)
+print(my_list[6][1])
+
+my_list = [10, 3, 8, 9, 42, "hello", [5, 4, 2, "world"]]
+print(my_list)
+print(my_list[6][2:])
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
+
+my_list.insert(2, 4)
+
+print(*my_list)
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list.index(3))
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
+
+my_list.append(22)
+print(*my_list)
+
+my_list =[10, 3, 8, 9, 42, 8, "hello"]
+print(my_list.count(8))
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+
+print(my_list)
+print(my_lsit.pop())
+print(*my_list)
+
+my_list = [10, 3, 8, 9, 42, 8]
+
+print(my_list)
+my_lsit.sort()
+
+print(*my_list)
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
+my_list.sort()
+
+print(*my_list)
+
+my_list =[10, 3, 8, 9, 42, 8, "hello"]
+
+print(my_list)
+my_list.reverse()
+
+print(*my_list)
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+list_tmp = [5, 7, "world"]
+
+print(my_list, list_tmp)
+my_list.extend(list_tmp)
+print(*my_list)
+list_tmp.clear()
+print(list_tmp)
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
+
+my_list.remove(3)
+print(*my_list)
+
+my_tuple = (1, 2, 'three', True)
+tpItem = (3.14, "b" , 'two', False)
+
+my_tup =(4, 2, 6, 1, "py", "w")
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup)
+print(my_tup[2])
+print(*my_tup)
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup)
+print(my_tup[2])
+my_tup[3] = 2
+print(*my_tup)
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup)
+print(my_tup[2] + my_tup[0])
+print(*my_tup)
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup)
+n_add = my_tup[1] + my_tup[3]
+print(n_add)
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup.index(2, 0, 3))
+
+my_tup = (4, 2, 6, 1, "py". "w")
+print(my_tup.index("py", 3, 5))
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup.index(1, 0, 3))
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicData = {"name" : "pyhon", "number" : 23564897}
+
+my_dict = {'key1 : 'value1', 'key2': 'value2}
+my_item = my_dict.items()
+print(my_item)
+
+idx = ("key1", "key2", "key3")
+dicts = dict.fromkey(idx, "0")
+print(dicts)
+
+
+         
