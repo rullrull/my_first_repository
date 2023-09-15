3d855e56050e4f49c63f0bbd84d447a9ed1cfd29
rullrull <143788468+rullrull@users.noreply.github.com>
2023-09-15 16:35:20 +0900

    Add files via upload

---
+++ eddittest.py
@@ -0,0 +1,27 @@
+a = "hello"
+b = "world"
+print(a,b)
+
+print(a)
+
+print(b)
+
+print(b,a)
+
+print("python",a,b)
+
+number_school = 2394584903
+str_name = "python"
+print(number_school, str_name)
+
+bool_view = False 
+print(bool_view)
+
+fit_pi=3.14
+print(fit_pi)
+
+char_element = 'a'
+print(char_element)
+
+chars = "hello world"
+print(chars)
