5e2f2bd28c7583fe2eb7b0146dea7489e4116124
rullrull <143788468+rullrull@users.noreply.github.com>
2023-09-22 17:51:57 +0900

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+def mt_sqrt(x) :
+    return math.sqrt(x)
+
+def mt_sinpi() :
+    return math.sin(math.pi /2)
+
+def mt_elog() :
+    return math.log(math.e)
+
+def mt_exp(x) :
+    return math.exp(x)
+
+def mt_pi() :
+    return math.pi
+
+
+
+#=================================
