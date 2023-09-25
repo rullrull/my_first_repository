548f4809d5146b204012b1058df56ea01314d3ed
rullrull <143788468+rullrull@users.noreply.github.com>
2023-09-25 17:52:21 +0900

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,22 @@
+def add(a, b) :
+    print("a + b :", a + b)
+    return a + b
+def sub(a, b) :
+    print("a - b :", a - b)
+    return a - b
+def mul(a, b) :
+    print("a * b :", a * b)
+    return a * b
+def div(a, b) :
+    print("a / b :", a / b)
+    return a / b
+
+#==================================
+#사용가능 메소드
+'''
+import calc
+
+print(dir(clac))
+
+#c
+'''
