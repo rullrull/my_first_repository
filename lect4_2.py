3ffc5d85908b40b43ef91073b0368db6ed70b37f
rullrull <143788468+rullrull@users.noreply.github.com>
2023-09-25 17:51:34 +0900

    Add files via upload

---
+++ lect4_2.py
@@ -0,0 +1,166 @@
+#콜백함수
+'''
+def prt_func() :
+    print("hello")
+ 
+def callfunc(fx) :
+    fx()
+    
+callfunc(prt_func)
+'''
+'''
+def prt_func(n) :
+    print("hello", n)
+ 
+def callfunc(fx) :
+    for i in range(5) :
+        fx(i)
+    
+callfunc(prt_func)
+'''
+
+#타입힌트
+'''
+def update_msg(name: str) -> list :
+    msg =[]
+    msg.append("hello," + name)
+    msg.append("bye, "+ name)
+    return msg
+
+def greeting(in_name: str) -> list :
+    gt_msg =None
+    gt_msg =update_msg(in_name)
+    return gt_msg
+
+res = greeting("pyhon")
+
+for message in res:
+    print(message)
+'''
+'''
+def fun(n) :
+    if n == 5 :
+        return
+        
+    print(1, n)
+    fun(n+1)
+    #print(2,n)
+    
+fun(1)
+'''
+'''
+def ploop(n) :
+    if n == 0:
+        print("end")
+        return 1
+    else : 
+        print(n, n-1, "=", n+n-1)
+        return n * ploop(n-1)
+
+print(ploop(3)) 
+'''
+'''
+def fibonacci(n):
+    if n == 0:
+        return 1
+    else:
+        print(n)
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+res = fibonacci(4)
+print("res = ", res)
+'''
+
+
+#==================================
+#사용가능 메소드
+'''
+import calc
+
+print(dir(clac))
+'''
+#calc 모듈 호출
+
+import calc as cl
+#res = calc,add(8,4)
+#print(res)
+print(cl.add(8, 4))
+print(cl.sub(8, 4))
+print(cl.mul(8, 4))
+print(cl.div(8, 4))
+#alias 사용
+
+
+#circle mod
+
+import mod.circle_mod as cm
+
+print(cm.pi)
+
+print(cm.cc_area(4))
+print(cm.cc_len(5))
+
+
+
+#문자유틸 모듈화
+
+import mod.str_util as smod
+
+url = "https://jtiger.notion.site/b92afb37d5fd4c65a297a15ebaaa472e?v=f0c783bc1b47461ab7668cc02f4e272e&p=7becb862e478405c85d006699a283c3b&pm=c"
+res = smod.cutstr(url, "/", 3)
+print(res)
+
+
+# math 모듈 사용
+import math
+sq_res = math.sqrt(6)
+print(sq_res)
+
+sq_res = math.sin(math.pi /2)
+print(sp_res)
+
+#math 모듈 사용
+import math
+sq_res = math.sqrt(6)
+print(sq_res)
+sq_res = math.sin(math.pi/2)
+print(sq_res)
+sq_res = math.log(math.e)
+print(sq_res)
+sq_res = math.exp(3)
+print(sq_res)
+pi_res = math.pi
+print(pi_res)
+
+import mod.utils as mu
+
+res = mu.mt_sqrt(7)
+print(res)
+
+sin = mu.mt_sinpi()
+print(sin)
+
+el = mu.mt_elog()
+print(el)
+
+ep = mu.mt_exp(3)
+print(ep)
+
+pi = mu.mt_pi()
+print(pi)
+
+
+import random as rd
+
+res = rd.randint(1,100)
+print(res)
+
+my_list =["apple", "banana", "cherry"]
+lres = rd.choice(my_list)
+print(lres)
+
+fres=rd.random()
+print(fres)
+
+nvres = rd.normalvariate()
+print(nvres)
